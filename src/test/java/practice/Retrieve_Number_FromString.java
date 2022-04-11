fa7785ac5a56e4dfcef7b2d6cd5ebf76ef5fd7ce
bhask <bhask@DESKTOP-PA2MCSP>
2022-04-11 19:02:15 +0530

    First

--- src/test/java/practice/Retrieve_Number_FromString.java
+++ src/test/java/practice/Retrieve_Number_FromString.java
@@ -11,6 +11,6 @@ public class Retrieve_Number_FromString {
 		System.out.println("The number part in str1 is "+" "+str1.replaceAll("[^0-9]",""));
 		
 		System.out.println("The Number part in str2 is "+" "+Str2.replaceAll("[^0-9]", ""));
-	}
+	}						
 
 }
