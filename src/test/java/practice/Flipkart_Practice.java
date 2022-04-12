16d12af29e9ecd60a70a51ec3f7607e36ab54754
bhask <bhask@DESKTOP-PA2MCSP>
2022-04-12 11:43:58 +0530

    iuewiufewfu`

---
+++ src/test/java/practice/Flipkart_Practice.java
@@ -0,0 +1,31 @@
+package practice;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Flipkart_Practice {
+	
+	static WebDriver driver;
+
+	public static void main(String[] args) {
+		driver=new ChromeDriver();
+		driver.get("https://www.amazon.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']")).sendKeys("iphone",Keys.ENTER);
+		String str=driver.findElement(By.xpath("//div[@cel_widget_id='UPPER-RESULT_INFO_BAR-0']/h1/div/div/div/div/span")).getText();
+		System.out.println(str);
+		
+	  String [] strarr=str.split(" ");
+	  System.out.println(strarr[2]);
+		
+		
+		
+		}
+
+}
