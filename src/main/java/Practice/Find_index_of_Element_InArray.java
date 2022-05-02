5848ec25f80e0cbc9b4ee56bceaa822ad13bb713
chappidi127 <bhaskar.chappidi127@gmail.com>
2022-05-02 12:03:56 +0530

    Total 13 prints

--- src/main/java/Practice/Find_index_of_Element_InArray.java
+++ src/main/java/Practice/Find_index_of_Element_InArray.java
@@ -27,6 +27,9 @@ public class Find_index_of_Element_InArray {
 		System.out.println("Index ="+index);
 		System.out.println("Index ="+index);
 		System.out.println("Index ="+index);
+		System.out.println("Index ="+index);
+		System.out.println("Index ="+index);
+		System.out.println("Index ="+index);
 		
 		
 		
