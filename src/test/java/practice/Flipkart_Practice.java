7fc637ebf128a5f4aca486a2ccf8d5e27402d1e8
chappidi127 <bhaskar.chappidi127@gmail.com>
2022-05-08 11:01:36 +0530

    first shiva commit

--- src/test/java/practice/Flipkart_Practice.java
+++ src/test/java/practice/Flipkart_Practice.java
@@ -24,7 +24,7 @@ public class Flipkart_Practice {
 	  String [] strarr=str.split(" ");
 	  System.out.println("Test");
 	  System.out.println(strarr[2]);
-	  System.out.println("Testing for shiva");
+	  System.out.println("Testing for shivadgfdg");
 		System.out.println("Testing for Bhaskar");
 		
 		
