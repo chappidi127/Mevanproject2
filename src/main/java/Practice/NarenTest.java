e87aa2b4e639976d18a657e34e3a7e3af2a2eb62
chappidi127 <bhaskar.chappidi127@gmail.com>
2022-05-11 11:10:04 +0530

    naren commit

---
+++ src/main/java/Practice/NarenTest.java
@@ -0,0 +1,13 @@
+package Practice;
+
+public class NarenTest {
+
+	public static void main(String[] args) {
+		System.out.println("Naren test");
+		System.out.println("Naren test");
+		System.out.println("Naren test");
+		System.out.println("Naren test");
+
+	}
+
+}
